f039b55a5ec0fe31c98bc633f71d2747f0983a27
Tung Nguyen <intern@kyc.net.vn>
2019-11-26 22:37:09 +0700

    ok

--- index.js
+++ index.js
@@ -32,13 +32,12 @@ const getCategories = async (inputLink) => {
                     });
                 });
             }
-            let CategoryImg = document.querySelector('.page_catalog img') ? document.querySelector('.page_catalog img').getAttribute('src') : document.querySelector('.catalog_banner img').getAttribute('src');
-            let CategoryName = document.querySelector('.page_catalog .intro h1') ? document.querySelector('.page_catalog .intro h1').innerText : document.querySelector('.catalog_banner .intro p').innerText;
-            // let CategoryName =''
-            // let description = ''
-            let description = document.querySelector('.page_catalog .content') ? document.querySelector('.page_catalog .content').innerText :
-                (document.querySelector('.page_catalog .intro div') ? document.querySelector('.page_catalog .intro div').innerText : document.querySelector('.page_catalog .intro span').innerText);
-            return { Name: CategoryName, OriginLink: originLink, Img: CategoryImg, Description: description, ChildCategoriesLinks: childCategoriesLinks }
+            // let CategoryImg = document.querySelector('.page_catalog img') ? document.querySelector('.page_catalog img').getAttribute('src') : document.querySelector('.catalog_banner img').getAttribute('src');
+            // let CategoryName = document.querySelector('.page_catalog .intro h1') ? document.querySelector('.page_catalog .intro h1').innerText : document.querySelector('.catalog_banner .intro p').innerText;
+            // let description = document.querySelector('.page_catalog .content') ? document.querySelector('.page_catalog .content').innerText :
+            //     (document.querySelector('.page_catalog .intro div') ? document.querySelector('.page_catalog .intro div').innerText : document.querySelector('.page_catalog .intro span').innerText);
+            // return { Name: CategoryName, OriginLink: originLink, Img: CategoryImg, Description: description, ChildCategoriesLinks: childCategoriesLinks }
+            return { OriginLink: originLink, ChildCategoriesLinks: childCategoriesLinks}
         });
 
         // console.log('Category:', results)
@@ -63,7 +62,7 @@ const getCategories = async (inputLink) => {
 }
 const getProducts = async (link) => {
     try {
-        let browser2 = await puppeteer.launch({ headless: false });
+        let browser2 = await puppeteer.launch();
         let page2 = await browser2.newPage();
         let finalProductList = [];
         await page2.goto(link, { waitUntil: 'load', timeout: 0 });
@@ -82,14 +81,7 @@ const getProducts = async (link) => {
             }
             if (document.querySelector('.pagination li')) {
                 maxPage = document.querySelectorAll('.pagination a')[document.querySelectorAll('.pagination a').length - 2].innerText;
-                // if (curPage <= maxPage && document.querySelector(`[href="javascript:gotopage(${curPage})"]`)) {
-                //     document.querySelector(`[href="javascript:gotopage(${curPage})"]`).click();
-                //     createData();
-                //     curPage++;
-                // } else if(curPage == 1) {
                 createData();
-                //     curPage++;
-                // }
                 return { products: products, maxPage: maxPage }
             } else {
                 createData();
@@ -100,10 +92,8 @@ const getProducts = async (link) => {
         finalProductList.push(...productsResult.products);
         if (productsResult.maxPage > 1) {
             for (let i = 2; i <= productsResult.maxPage; i++) {
-                // browser2 = await puppeteer.launch({ headless: false });
-                // page2 = await browser2.newPage();
-                // await page2.goto(link, { waitUntil: 'load', timeout: 0 });
                 let nextPage = await browser2.newPage();
+                nextPage.setDefaultNavigationTimeout(0); 
                 await nextPage.goto(link, { waitUntil: 'load', timeout: 0 });
                 await nextPage.$eval(`[href="javascript:gotopage(${i})"]`, elem => elem.click());
                 await nextPage.waitForNavigation();
@@ -119,16 +109,16 @@ const getProducts = async (link) => {
                     });
                     return products;
                 });
-                console.log(`PAGE ${i} RESULTS:`, pagesResult.length);
+                // console.log(`PAGE ${i} RESULTS:`, pagesResult.length);
                 finalProductList.push(...pagesResult)
             }
         }
         await browser2.close();
         // allProducts.push(...productsResult)
-        console.log('Return:', productsResult);
+        // console.log('Return:', productsResult);
         console.log('Final Products List:', finalProductList.length);
-        for (let i = 0; i < productsResult.length; i++) {
-            // await getProductInfo(productsResult[i].link);
+        for (let i = 0; i < finalProductList.length; i++) {
+            await getProductInfo(finalProductList[i].link);
         }
     } catch (error) {
         console.log(link);
@@ -167,6 +157,7 @@ const getProductInfo = async (link) => {
             product.Cat_id = document.querySelectorAll('.path a')[document.querySelectorAll('.path a').length - 2].innerText;
             product.Color = generalInfo[0].querySelectorAll('ul li')[1].querySelectorAll('span')[1].innerText;
             product.Price = productSelector.querySelector('.price-txt').innerText;
+            product.Des = document.querySelector(`#tinhnang`).innerText;
             return product;
         });
         console.log(`${count}-Product Detail:`, productInfo);
@@ -179,7 +170,7 @@ const getProductInfo = async (link) => {
 }
 (async () => {
     // getCategories('http://khoahuyhoang.net/san-pham-danh-cho-cua-go/');
-    // getCategories('http://khoahuyhoang.net/khoa-tay-nam-hc/khoa-hc-85/');
+    // getCategories('http://khoahuyhoang.net/san-pham-danh-cho-cua-go/khoa-tay-nam/');
     // getProducts('http://khoahuyhoang.net/khoa-tay-nam/khoa-tay-nam-huy-hoang-tieu-chuan-chau-au/');
     getProducts('http://khoahuyhoang.net/khoa-tay-nam-hc/khoa-hc-85/');
     // getProductInfo('http://khoahuyhoang.net/tay-nam-cua-jaguar-pvd-gold/TNCHCJAG-PVDGO.html')
